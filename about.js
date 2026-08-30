c8af323849bf62b2689e1a3cbe1ac53d49d65707
olotuah joseph <olotuahtobiloba@gmail.com>
2026-08-30 10:18:53 -0700

    Initial portfolio website

---
+++ about.js
@@ -0,0 +1,57 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    // ================= SELECT ELEMENTS =================
+
+    const hamburger = document.getElementById("hamburger");
+
+    const mobileMenu = document.getElementById("mobileMenu");
+
+    const closeMenu = document.getElementById("closeMenu");
+
+
+
+    // ================= OPEN MENU =================
+
+    hamburger.addEventListener("click", () => {
+
+        mobileMenu.classList.add("active");
+
+    });
+
+
+
+    // ================= CLOSE MENU =================
+
+    closeMenu.addEventListener("click", () => {
+
+        mobileMenu.classList.remove("active");
+
+    });
+
+
+
+    // ================= CLOSE MENU WHEN SCROLLING =================
+
+    window.addEventListener("scroll", () => {
+
+        mobileMenu.classList.remove("active");
+
+    });
+
+
+
+    // ================= CLOSE MENU WHEN A LINK IS CLICKED =================
+
+    const menuLinks = document.querySelectorAll(".mobile-menu a");
+
+    menuLinks.forEach(link => {
+
+        link.addEventListener("click", () => {
+
+            mobileMenu.classList.remove("active");
+
+        });
+
+    });
+
+});
